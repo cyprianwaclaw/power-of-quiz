d0d18b51fcf8178a3c5bcf367295f40bb28a34f4
Cyprian Waclaw <cyprianwaclaw@gmail.com>
2023-08-19 14:03:49 +0200

    test

--- pages/panel/quiz/index.vue
+++ pages/panel/quiz/index.vue
@@ -178,24 +178,23 @@ const pagination = () => {
 // };
 const pagination1 = () => {
   const lastPageNumber = allQuiz.value.last_page;
+  const halfPageToShow = Math.floor(pageToShow / 2);
 
-  // Oblicz indeks początkowy i końcowy stron, tak aby wyświetlać 5 stron z aktywnym środkiem.
-  let startPage = Math.max(1, currentPage.value - Math.floor(pageToShow / 2));
-  let endPage = Math.min(lastPageNumber, startPage + pageToShow - 1);
+  const startPage = Math.max(1, currentPage.value - halfPageToShow);
+  const endPage = Math.min(lastPageNumber, startPage + pageToShow - 1);
 
-  // Dostosuj indeks początkowy, jeśli końcowa strona jest za blisko końca.
-  if (endPage - startPage + 1 < pageToShow) {
-    startPage = Math.max(1, endPage - pageToShow + 1);
-  }
-  // Aktualizuj tablicę linksPagination z pełnymi informacjami o stronach.
-  allQuiz.value.links = [];
-  for (let i = startPage; i <= endPage; i++) {
-    const isActive = i === currentPage.value; // Sprawdza, czy strona jest aktywna
-    allQuiz.value.links.push({ label: i, url: `?page=${i}`, active: isActive });
-  }
+  allQuiz.value.links = Array.from({ length: endPage - startPage + 1 }, (_, i) => {
+    const pageNumber = startPage + i;
+    return {
+      label: pageNumber,
+      url: `?page=${pageNumber}`,
+      active: pageNumber === currentPage.value,
+    };
+  });
 
   return allQuiz.value.links;
 };
+
 
 const changePageButton = async (value: any) => {
   currentPage.value = value;
